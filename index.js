4844571cb0d5bed1e66358af3cf67b0ec318b1c7
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 15:50:11 +0800

    啊啊啊

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-return true;
+var crypto=require("crypto");
+var isLegel = function (signature, timestamp, nonce) {
+    var TOKEN = 'luluwechat';
+    var arr = [TOKEN, timestamp, nonce];
+    // 对三个参数进行字典序排序
+    arr.sort();
+    // sha1 加密
+    var sha1 = crypto.createHash('sha1');
+    var msg = arr[0] + arr[1] + arr[2];
+    sha1.update(msg);
+    msg = sha1.digest('hex');
+    // 验证
+    if(msg == signature) {
+        console.log('验证成功');
+        return true;
+    } else {
+        console.log('验证失败');
+        return false;
+    }
+};
