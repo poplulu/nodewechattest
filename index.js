95c916efa2356b9e4b818764d50b09f9c7620747
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 16:15:02 +0800

    v

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-var express = require('express')
 var crypto = require('crypto')
-var app = express();
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
 
 var isLegel = function (signature, timestamp, nonce) {
     var TOKEN = 'luluwechat';
