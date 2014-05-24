839434f93cda3864d0b06b7d57b0b67fa15f6a99
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 16:46:56 +0800

    last

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-var crypto = require('crypto')
+var crypto = require('crypto');
+var express = require('express'),
+    app = express();
 
-var isLegel = function (signature, timestamp, nonce) {
-    var TOKEN = 'luluwechat';
-    var arr = [TOKEN, timestamp, nonce];
-    // 对三个参数进行字典序排序
-    arr.sort();
-    // sha1 加密
-    var sha1 = crypto.createHash('sha1');
-    var msg = arr[0] + arr[1] + arr[2];
-    sha1.update(msg);
-    msg = sha1.digest('hex');
-    // 验证
-    if(msg == signature) {
-        console.log('验证成功');
-        return true;
+// 解析器
+app.use(express.bodyParser());
+
+// 接入验证
+app.get('/', functionvalidateToken {
+
+    // 签名成功
+    if (validateToken(req, res)) {
+        res.send(200, req.query.echostr);
     } else {
-        console.log('验证失败');
-        return false;
+        res.send(200, 'fail');
     }
-};
+});
+
+function sha1(str) {
+    var md5sum = crypto.createHash('sha1');
+    md5sum.update(str);
+    str = md5sum.digest('hex');
+    return str;
+}；
+
+function validateToken(req, res) {
+    var query = req.query;
+    var signature = query.signature;
+    var echostr = query.echostr;
+    var timestamp = query['timestamp'];
+    var nonce = query.nonce;
+    var oriArray = new Array();
+    oriArray[0] = nonce;
+    oriArray[1] = timestamp;
+    oriArray[2] = "luluwechat";//这里填写你的token
+    oriArray.sort();
+    var original = oriArray[0]+oriArray[1]+oriArray[2];
+    console.log("Original Str:"+original);
+    console.log("signature:"+signature);
+    var scyptoString = sha1(original);
+    if (signature == scyptoString) {
+        res.send(echostr);
+    }
+    else {
+        res.send("Bad Token!");
+    }
+}
