f5eaa212e6292a07daa3528e131022eadd66ca40
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 12:57:18 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express')
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-var port = Number(process.env.PORT || 50);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
