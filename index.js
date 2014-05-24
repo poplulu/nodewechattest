1784ac5822a45e70cf2a8ba1a7c8b158e58c01a1
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 15:42:35 +0800

    222

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var wechat = require('wechat');
-
-app.use(connect.query()); // Or app.use(express.query());
-app.use('/wechat', wechat('luluwechat', function (req, res, next) {
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
-  res.reply('hehe');
-  }));
+return true;
