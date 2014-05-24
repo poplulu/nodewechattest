1da04cc802ca5412913de8568f8946f3b585c79a
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 16:12:32 +0800

    3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var crypto = require('crypto')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -13,3 +14,22 @@ app.listen(app.get('port'), function() {
 })
 
 
+var isLegel = function (signature, timestamp, nonce) {
+    var TOKEN = 'luluwechat';
+    var arr = [TOKEN, timestamp, nonce];
+    // 对三个参数进行字典序排序
+    arr.sort();
+    // sha1 加密
+    var sha1 = crypto.createHash('sha1');
+    var msg = arr[0] + arr[1] + arr[2];
+    sha1.update(msg);
+    msg = sha1.digest('hex');
+    // 验证
+    if(msg == signature) {
+        console.log('验证成功');
+        return true;
+    } else {
+        console.log('验证失败');
+        return false;
+    }
+};
