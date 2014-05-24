87e108f0a84ba1831bd8d557c9ff11eaeca7d87c
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 13:57:33 +0800

    3

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var wechat = require('./lib/wechat');
-wechat.List = require('./lib/list');
-wechat.API = require('./lib/common');
-wechat.OAuth = require('./lib/oauth');
+var wechat = require('wechat');
 module.exports = wechat;
 
 var welcome = require('./tianyang/welcome');
