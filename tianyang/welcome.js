8ce364d47dc3a0b59e1e53d9fd35eb142d383d84
Manny Wang <poplulu.dalian@gmail.com>
2014-05-24 14:27:54 +0800

    45

--- tianyang/welcome.js
+++ tianyang/welcome.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('微信公众平台 测试!')
+  response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
